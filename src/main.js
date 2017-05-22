a6d90b50e3e56596b786c378a774b4a230cbc87b
Duncan De Weireld <duncan.deweireld@student.umons.ac.be>
2017-05-22 19:50:39 +0200

    moved index to avoid going up in directories

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
  */
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var app_module_1 = require("./app/app.module");
+var app_module_js_1 = require("./app/app.module.js");
 var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_js_1.AppModule);
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * Created by helldog136 on 22/03/17.
  */
 
-import { AppModule }             from './app/app.module';
+import { AppModule }             from './app/app.module.js';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
