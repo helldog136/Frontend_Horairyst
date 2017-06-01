6b4aee843818765482bd6f322e43c9bf12edc7bb
root <root@vps412326.ovh.net>
2017-06-01 17:03:00 +0200

    adding script to launch as daemon

---
+++ start_server.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+npm run serve
+
+
+# start : daemonize -o /var/log/horairyst_frontend.log -e /var/log/horairyst_frontend.log -c /var/www/Frontend_Horairyst /var/www/Frontend_Horairyst/start_server.sh
